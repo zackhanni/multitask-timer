5ad15157a32a2fa4a6a2bc6046ccb9a211322f5b
Zack Hanni <zack.hannI@gmail.com>
2024-10-29 16:43:02 -0400

    removed underline on my name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <a className="flex items-center gap-2 hover:underline hover:underline-offset-4">
+      <a className="flex items-center gap-2">
         {/* <Image
           aria-hidden
           src="/file.svg"
