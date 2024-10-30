9c77e54fb654baf48517239e34f180890c505041
Zack Hanni <zack.hannI@gmail.com>
2024-10-30 15:36:00 -0400

    removed unused. removed grid

--- app/page.tsx
+++ app/page.tsx
@@ -2,41 +2,17 @@ import Footer from "@/components/Footer";
 import Stopwatches from "@/components/Stopwatches";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold dark:invert">Multitask Timer</h1>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] space-y-2">
-          <li>Name and create your timer.</li>
-          <li>Only 1 timer will run at a time.</li>
-          <li>You can rename, reset or delete your timers.</li>
-        </ol>
+    <div className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="space-y-10">
+        <div className="container flex flex-col space-y-8 items-center sm:items-start">
+          <h1 className="text-4xl font-bold dark:invert">Multitask Timer</h1>
+          <ol className="list-inside list-decimal text-center sm:text-left font-[family-name:var(--font-geist-mono)] space-y-2">
+            <li>Name and create your timer.</li>
+            <li>When 1 timer starts, others stop.</li>
+            <li>You can rename, reset or delete your timers.</li>
+          </ol>
+        </div>
         <Stopwatches />
-
-        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div> */}
       </main>
       <Footer />
     </div>
