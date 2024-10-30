f684674029cd6282b5a3d745db0b7ceb204a2296
Zack Hanni <zack.hannI@gmail.com>
2024-10-30 15:44:53 -0400

    change icon colors on hover

--- components/Stopwatches.tsx
+++ components/Stopwatches.tsx
@@ -185,6 +185,11 @@ export default function Stopwatches() {
                   <Button
                     variant="outline"
                     size="icon"
+                    className={`${
+                      timer.isRunning
+                        ? "hover:bg-yellow-100 hover:border-yellow-500"
+                        : "hover:bg-green-100 hover:border-green-500"
+                    }`}
                     onClick={() => toggleTimer(timer.id)}
                   >
                     {timer.isRunning ? (
@@ -196,6 +201,7 @@ export default function Stopwatches() {
                   <Button
                     variant="outline"
                     size="icon"
+                    className="hover:bg-blue-100 hover:border-blue-500"
                     onClick={() => resetTimer(timer.id)}
                   >
                     <RefreshCw className="h-4 w-4" />
@@ -205,6 +211,7 @@ export default function Stopwatches() {
                       <Button
                         variant="outline"
                         size="icon"
+                        className="hover:bg-red-100 hover:border-red-500"
                         onClick={() => setTimerToDelete(timer.id)}
                       >
                         <Trash2 className="h-4 w-4" />
