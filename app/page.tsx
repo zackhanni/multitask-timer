656e239a07191450b528f29f7ddf5ed1ed9b4096
Zack Hanni <zack.hannI@gmail.com>
2024-10-30 16:51:02 -0400

    added padding to <main>

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Stopwatches from "@/components/Stopwatches";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="space-y-10">
+      <main className="space-y-10 py-10">
         <div className="container flex flex-col space-y-8 items-center sm:items-start">
           <h1 className="text-4xl font-bold dark:invert text-accent">
             Multitask Timer
